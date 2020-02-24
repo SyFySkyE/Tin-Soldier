64dc07fa568a3b6faee1055d2cf772bd43fbab16
Chris Grych <ceggrych@gmail.com>
2020-02-24 00:44:33 -0600

    Finishes Milestone 2

---
+++ Assets/Scripts/Rock/Rock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    private Material currentMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentMaterial.color = Color.Lerp(Color.gray, Color.black, Mathf.PingPong(Time.time, 1));
+    }
+}
