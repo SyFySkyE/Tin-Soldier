5363ef9c6bc8262f3348e21f8bf099c68a6d1518
Chris Grych <ceggrych@gmail.com>
2020-04-07 11:43:33 -0500

    Adds new ship door plus anims

--- Assets/Scripts/InteractiveObjects/Door.cs
+++ Assets/Scripts/InteractiveObjects/Door.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Door : InteractiveObject
 {
+    [Header("SFX for Door Closing")]
+    [SerializeField] private AudioClip doorCloseSfx;
+
+    [Header("If door should close, what trigger should open it?")]
+    [Tooltip("Leave empty to leave door open")]
+    [SerializeField] private PlayerUnityTrigger closeTrigger;
+
     private Animator doorAnim;
     private int doorOpenAnimParameter = Animator.StringToHash("DoorOpen");
 
@@ -17,6 +24,18 @@ public class Door : InteractiveObject
     {
         base.Awake();
         doorAnim = GetComponent<Animator>();
+        if (closeTrigger != null)
+        {
+            closeTrigger.OnTrigger += CloseTrigger_OnTrigger;
+        }
+    }
+
+    private void CloseTrigger_OnTrigger()
+    {        
+        if (doorAnim.GetBool(doorOpenAnimParameter))
+        {
+            doorAnim.SetBool(doorOpenAnimParameter, false);
+        }
     }
 
     public override void Interact()
