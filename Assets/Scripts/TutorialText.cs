d80ea454af3dd034ce989ed4f763151297706031
Chris Grych <ceggrych@gmail.com>
2020-05-04 15:26:30 -0500

    Starts tutorial text

--- Assets/Scripts/TutorialText.cs
+++ Assets/Scripts/TutorialText.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Runtime.CompilerServices;
+
+public enum Tutorials
+{
+    Interact,
+    Read,
+    Inventory,
+    ExitInventory,
+    Sprint,
+    LockedDoor,
+    Jetpack
+}
 
 public class TutorialText : MonoBehaviour
 {
     [SerializeField] private string interactText = "You can interact with certain objects by targeting them and pressing \"E\"";
-    [SerializeField] private string lockedText = "This door requires a key card to enter";
+    [SerializeField] private string readText = "Press \"E\" again to stop reading";
+    [SerializeField] private string inventoryText = "Access your internal inventory by pressing \"Tab\" or \"I\"";
+    [SerializeField] private string exitInventoryText = "Exit by pressing \"Tab\" or \"I\" again";
+    [SerializeField] private string sprintText = "Hold down \"Left Shift\" to sprint";
+    [SerializeField] private string lockedText = "You need to find a badge with elevated privileges to open this door.";
+    [SerializeField] private string jetpackText = "You've equipped a jetpack. Press Space to jump, and hold space while jumping to ascend.";
+    [SerializeField] private string energyUsageText = "Your jetpack energy is represented by a meter. If it empties, you will fall. It recharges when not in use.";
 
     private TextMeshProUGUI tutorialText;
     private Animator textAnim;
+
+    public static TutorialText Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+    private static TutorialText instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +53,32 @@ public class TutorialText : MonoBehaviour
         tutorialText = GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayTutorialText(Tutorials tutorialToPlay)
     {
-        
+        switch (tutorialToPlay)
+        {
+            case Tutorials.Interact:
+                tutorialText.text = interactText;
+                break;
+            case Tutorials.Inventory:
+                tutorialText.text = inventoryText;
+                break;
+            case Tutorials.Read:
+                tutorialText.text = readText;
+                break;
+            case Tutorials.ExitInventory:
+                tutorialText.text = exitInventoryText;
+                break;
+            case Tutorials.Sprint:
+                tutorialText.text = sprintText;
+                break;
+            case Tutorials.Jetpack:
+                tutorialText.text = jetpackText;
+                break;
+            case Tutorials.LockedDoor:
+                tutorialText.text = lockedText;
+                break;
+        }
+        textAnim.SetTrigger("Tutorial");
     }
 }
