a56b778cd55b256ebf5aa5578e3a8e67d825108f
Chris Grych <ceggrych@gmail.com>
2020-05-09 11:40:08 -0500

    Starts adding more footstep sounds

--- Assets/Asset Packs/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Asset Packs/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -26,8 +26,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private LerpControlledBob m_JumpBob = new LerpControlledBob();
         [SerializeField] private float m_StepInterval;
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
-        [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
-        [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+        [SerializeField] private AudioClip[] m_JumpSound;           // the sound played when character leaves the ground.
+        [SerializeField] private AudioClip[] m_LandSound;           // the sound played when character touches back on ground.
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -171,7 +171,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayLandingSound()
         {
-            m_AudioSource.clip = m_LandSound;
+            m_AudioSource.clip = m_LandSound[Random.Range(0, m_LandSound.Length)];
             m_AudioSource.Play();
             m_NextStep = m_StepCycle + .5f;
         }
@@ -233,7 +233,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayJumpSound()
         {
-            m_AudioSource.clip = m_JumpSound;
+            m_AudioSource.clip = m_JumpSound[Random.Range(0, m_JumpSound.Length)];
             m_AudioSource.Play();
         }
 
